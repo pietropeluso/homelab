95372735908ed193500211c67138ba4f1c34775a
Pietro Peluso <pietro.peluso@gmail.com>
2025-03-29 20:12:02 +0000

    adding auto installation flag

--- get-docker.sh
+++ get-docker.sh
@@ -14,7 +14,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # check docker version
 docker -v
